f1ee47de9ae4c79985861fcd26c5fab31e403879
Stan <stasostaszewski@gmail.com>
2022-12-03 17:11:11 +0100

    display data back to original

--- displayData.py
+++ displayData.py
@@ -1,20 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def correct_type(data):
-    corrected = np.zeros([data.shape[0], data.shape[1]])
-    for y in range(data.shape[0]):
-        for x in range(data.shape[1]):
-            corrected[y, x] = float(data[y, x])
-    return corrected
-
 def displayData(data):
     fig = plt.figure()
     plt.gray()
     for i in range(data.shape[0]):
         subplot_dimensions = int(np.ceil(np.sqrt(data.shape[0])))
         ax = fig.add_subplot(subplot_dimensions, subplot_dimensions, i+1)
-        reshaped = correct_type(np.reshape(data[i], (-1, 20)))
-        ax.imshow(reshaped.T)
+        ax.imshow(np.transpose(np.reshape(data[i,:], (-1, 20))))
         ax.set_axis_off()
     plt.show()
